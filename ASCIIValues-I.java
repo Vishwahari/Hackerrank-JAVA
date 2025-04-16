113f13d3ed8ddddf37b4c5e2818a655961c5f69a
Vishwahari R <140313931+Vishwahari@users.noreply.github.com>
2025-04-16 22:32:22 +0530

    Input & Output - ASCII Values - I

---
+++ ASCIIValues-I.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        char ch = sc.next().charAt(0);
+        System.out.println((int)ch);
+    }
+}
